86df6112b58b9e52fcddc1c2608e38e3eb5d7c88
adammuflihun <96274551+adammuflihun@users.noreply.github.com>
2023-06-08 11:17:14 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,53 @@ import { greetUser } from '$utils/greet';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  const name = 'John Doe';
-  greetUser(name);
-});
+  // const name = 'John Doe';
+  // greetUser(name);
+  console.log('Error occurred. Error code: ');
 
-// loop an element
-const elements = document.querySelectorAll('.my-element');
-elements.forEach((element) => {
-  // do something with elementll
+  // form location
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      function (position) {
+        var lat = position.coords.latitude;
+        var lng = position.coords.longitude;
+
+        var geocoder = new google.maps.Geocoder();
+        var latlng = new google.maps.LatLng(lat, lng);
+
+        geocoder.geocode(
+          {
+            latLng: latlng,
+          },
+          function (results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+              if (results[0]) {
+                for (var i = 0; i < results[0].address_components.length; i++) {
+                  var types = results[0].address_components[i].types;
+                  if (types.indexOf('postal_code') != -1) {
+                    var postalCode = results[0].address_components[i].short_name;
+                    console.log(postalCode);
+
+                    // Get the first element with a class of "address"
+                    const element = document.querySelector('.address');
+
+                    // Set its text content to the new address
+                    element.textContent = postalCode;
+
+                    break;
+                  }
+                }
+              }
+            } else {
+              console.log('Geocoder failed due to: ' + status);
+            }
+          }
+        );
+      },
+      function (error) {
+        console.log('Error occurred. Error code: ' + error.code);
+      }
+    );
+  }
+  // form location closing
 });
