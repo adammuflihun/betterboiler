a3382e9f4604361b133f93929a2c2779c50fc56c
adammuflihun <96274551+adammuflihun@users.noreply.github.com>
2023-06-08 17:07:07 +0800

    s

---
+++ src/utils/airtable.ts
@@ -0,0 +1,36 @@
+export const airtable = () => {
+  const token =
+    'patbind6rCod8yplJ.498983e06b091311d33044ed27b2585e5de01fca6c7dcb768573c3f615167c9c';
+  const baseUrl = 'https://api.airtable.com/v0';
+  const baseId = 'app4yR7Au4zhrkMxV'; // Update with your base ID
+
+  // Specify the table and any desired parameters
+  const tableName = 'betterboiler';
+  const params = {
+    filterByFormula: "SEARCH('MK4', {filterpostalcode})",
+    maxRecords: 3, // Limit the maximum number of records to 3
+  };
+
+  // Construct the URL
+  const url = `${baseUrl}/${baseId}/${tableName}?${new URLSearchParams(params)}`;
+
+  // Fetch the data
+  fetch(url, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      // Process the retrieved data
+      data.records.forEach((record) => {
+        console.log(record.fields.filterpostalcode);
+        console.log(record.fields.Name);
+        console.log(record.fields.Address);
+      });
+    })
+    .catch((error) => {
+      // Handle any errors
+      console.error(error);
+    });
+};
---
+++ src/utils/postcodecookie.ts
@@ -0,0 +1,17 @@
+export const postcodecookie = () => {
+  $(document).ready(function () {
+    // Function to save value in cookie consent
+    function saveToCookieConsent() {
+      var inputValue = $('[post="code"]').val(); // Get the value from the input element
+      // Save the value in a cookie
+      // Replace 'cookieName' with the desired name for your cookie
+      Cookies.set('cookieName', inputValue, { expires: 365, path: '/' });
+      console.log(Cookies.get('cookieName')); // Log the cookie value to the console
+    }
+
+    // Bind click event to the submit button
+    $('[data-form="submit-btn"]').click(function () {
+      saveToCookieConsent(); // Call the saveToCookieConsent function when the button is clicked
+    });
+  });
+};
