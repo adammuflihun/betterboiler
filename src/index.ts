9c128d48e96faba957326c8c0086d24edcc00d70
adammuflihun <96274551+adammuflihun@users.noreply.github.com>
2023-06-08 11:21:57 +0800

    s

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,6 @@ window.Webflow ||= [];
 window.Webflow.push(() => {
   // const name = 'John Doe';
   // greetUser(name);
-  console.log('mantap');
+  console.log('mantap from local');
+  Airtable.configure({ apiKey: 'patV8IqgXnN8CJ8kX' });
 });
